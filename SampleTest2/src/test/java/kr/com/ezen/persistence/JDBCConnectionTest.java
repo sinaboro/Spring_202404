1852ead29ce77191bab3607c306fbcf1a11443c2
daechul <rlaeocjf@gmail.com>
2024-04-05 09:42:11 +0900

    test

---
+++ SampleTest2/src/test/java/kr/com/ezen/persistence/JDBCConnectionTest.java
@@ -0,0 +1,31 @@
+package kr.com.ezen.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class JDBCConnectionTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try {
+			Connection con = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:xe",
+					"test2",
+					"1234"
+					);
+			log.info("con >> " +  con);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
