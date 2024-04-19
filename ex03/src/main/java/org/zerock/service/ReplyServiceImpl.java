3af515dc22f039c6e63db4ba22486ca08010be14
daechul <rlaeocjf@gmail.com>
2024-04-19 12:19:03 +0900

    test

---
+++ ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -0,0 +1,40 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+
+public class ReplyServiceImpl implements ReplyService{
+
+	@Override
+	public int register(ReplyVO reply) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ReplyVO register(Long rno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int modify(ReplyVO reply) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int remove(Long rno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Long bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
