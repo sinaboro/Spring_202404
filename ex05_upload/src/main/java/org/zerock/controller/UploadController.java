8d705f7edb5b18e5a061181cda5b8b94e45c1559
daechul <rlaeocjf@gmail.com>
2024-04-29 13:05:01 +0900

    test

--- ex05_upload/src/main/java/org/zerock/controller/UploadController.java
+++ ex05_upload/src/main/java/org/zerock/controller/UploadController.java
@@ -1,9 +1,12 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -73,14 +77,31 @@ public class UploadController {
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			log.info(uploadFileName);
+			
 			uploadFileName =  uploadFileName.substring(uploadFileName.lastIndexOf("\\")+1);
 			log.info(uploadFileName);
 			
+			UUID uuid = UUID.randomUUID();
+			
+			//파일명 앞에 래덤한 영문자 32자를 붙인다.  fafdfefeggfdafdfdasfdsfsfsdfdaf_01Apple.jpg
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			
 			File saveFile = new File(uploadPath, uploadFileName);  //저장 대상
 			
 			try {
 				multipartFile.transferTo(saveFile);  //파일 저장
+				
+				
+				if(checkImageType(saveFile)) {
+					
+					FileOutputStream thumbnail = new FileOutputStream(
+							new File(uploadPath, "s_" + uploadFileName)
+							);
+					
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(),thumbnail, 100,100 );
+					thumbnail.close();
+				}
+				
 			} catch (IllegalStateException | IOException e) {
 				e.printStackTrace();
 			}
@@ -98,4 +119,22 @@ public class UploadController {
 		
 		return str.replace("-", File.separator);  //  2024\04\29
 	}
+	
+	//이미지니? 
+	private boolean checkImageType(File file) {
+		
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");  //이미지파일 맞음
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false; //이미지파일 아니다.
+		
+	}
+	
+	
 }
