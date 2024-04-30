660f191b1450c3c65aafa7a8b8d8d7f05dde2cc6
daechul <rlaeocjf@gmail.com>
2024-04-30 14:29:04 +0900

    test

--- ex06_security2/src/main/java/org/zerock/controller/CommonController.java
+++ ex06_security2/src/main/java/org/zerock/controller/CommonController.java
@@ -18,6 +18,28 @@ public class CommonController {
 		log.info(auth);
 		
 		model.addAttribute("msg", "접근 권한이 없습니다.");
-		
 	}
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		
+		log.info("error : " + error);
+		log.info("logout : " + logout);
+		
+		if(error != null) {
+			model.addAttribute("error", "아이디 or 비번이 틀립니다.");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("logout", "로그아웃!!!!");
+		}
+	}
+	
+	@GetMapping("/customLogout")
+	public void logoutGET() {
+		log.info("custom logout.........");
+	}
+	
+	
+	
 }
